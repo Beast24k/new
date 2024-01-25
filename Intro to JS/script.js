60674a790f01611e2d8b2371bda171b1637265df
Beast24k <156655493+Beast24k@users.noreply.github.com>
2024-01-25 05:43:46 -0800

    Add files via upload

---
+++ Intro to JS/script.js
@@ -0,0 +1,22 @@
+//alert( "WELCOME")
+
+var name = "john"
+let a = 10
+let b =21
+const c = 30
+
+
+// alert(b%a)
+// alert (a/b)
+// alert(a-b)
+// alert(a+b)
+// alert(a*b)
+// alert(name)
+
+function onBulb() {
+document.getElementById("img").src= "pic_bulbon.gif"
+}
+
+function offBulb() {
+    document.getElementById("img").src= "pic_bulboff.gif"
+    }
